881284e2e23be3e8e6d7e98d951a5971a505f148
Batuhan Tomo <batuhan.tomo@gmail.com>
2024-02-09 12:27:19 +0300

    Add React and Vite setup with Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Banner from './sections/Banner/Banner'
+import Topbar from './sections/Topbar/Topbar'
+import Features from './sections/Features/Features'
+
+function App() {
+
+  return (
+    <>
+      <Topbar />
+      <Banner />
+      <Features />
+    </>
+  )
+}
+
+export default App
