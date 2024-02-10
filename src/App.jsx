3e4a7e5503e86edebfb88ece87c9e85eefb8355b
Batuhan Tomo <batuhan.tomo@gmail.com>
2024-02-10 17:23:25 +0300

    Add new sections to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import './App.css'
 import Banner from './sections/Banner/Banner'
 import Topbar from './sections/Topbar/Topbar'
 import Features from './sections/Features/Features'
+import Pricing from './sections/Pricing/Pricing'
+import Global from './sections/Global/Global'
+import Reviews from './sections/Reviews/Reviews'
+import Subscribe from './sections/Subscribe/Subscribe'
+import Footer from './sections/Footer/Footer'
 
 function App() {
 
@@ -10,6 +15,11 @@ function App() {
       <Topbar />
       <Banner />
       <Features />
+      <Pricing />
+      <Global />
+      <Reviews />
+      <Subscribe />
+      <Footer />
     </>
   )
 }
